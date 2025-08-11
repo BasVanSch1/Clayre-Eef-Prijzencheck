a326df70c1aeded77171daeff757667be499d772
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-08-11 12:40:47 +0200

    Added functionality to download product photos

--- app/routes/retrieveData.tsx
+++ app/routes/retrieveData.tsx
@@ -1,8 +1,34 @@
 import { fileStorage } from "~/services/filestorage.server";
 import type { Route } from "./+types/retrieveData";
 
-export async function loader({ params }: Route.LoaderArgs) {
-  const file = await fileStorage.get(params.id);
+export async function loader({ params, request }: Route.LoaderArgs) {
+  const url = new URL(request.url);
+  const product = url.searchParams?.get("product") === "true";
+  let file;
+
+  if (product) {
+    const response = await fetch(
+      `https://images.clayre-eef.com/lowres/${params.id}.jpg`
+    );
+
+    if (!response.ok) {
+      throw new Response("Product file not found", { status: 404 });
+    }
+
+    const contentType =
+      response.headers.get("content-type") || "application/octet-stream";
+    const contentDisposition =
+      response.headers.get("content-disposition") ||
+      `attachment; filename=${params.id.split("/").pop()}`;
+    file = {
+      stream: () => response.body,
+      type: contentType,
+      name: params.id.split("/").pop() || "download",
+      contentDisposition,
+    };
+  } else {
+    file = await fileStorage.get(params.id);
+  }
 
   if (!file) {
     throw new Response("File not found", {
