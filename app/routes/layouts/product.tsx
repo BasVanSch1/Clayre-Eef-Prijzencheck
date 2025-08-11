3e49bcbbe934afe9f56e7d6d2c91e1819c690d11
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-08-11 13:06:15 +0200

    styling update

--- app/routes/layouts/product.tsx
+++ app/routes/layouts/product.tsx
@@ -3,10 +3,10 @@ import LookupCard from "~/components/LookupCard";
 
 export default function ProductLayout() {
   return (
-    <div className="col-start-3 row-start-3 flex flex-col mt-5">
+    <div className="col-start-3 row-start-3 flex flex-col mt-5 gap-y-1">
       <Outlet />
 
-      <hr className="my-2 hidden h-px border-0 bg-gray-400/40 md:block" />
+      <hr className="my-1 hidden h-px border-0 bg-gray-400/40 md:block" />
 
       {/* Lookup field */}
       <div>
