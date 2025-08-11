8b246fb67d5bcdd4bf7e49b2bbf84828675d2146
BasVanSch1 <8457098+BasVanSch1@users.noreply.github.com>
2025-08-11 13:02:38 +0200

    add download product photo button

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,3 +1,4 @@
+import { DownloadIcon } from "./Icons";
 import type { Product } from "./Types";
 
 interface ProductCard {
@@ -5,11 +6,26 @@ interface ProductCard {
 }
 
 const ProductCard = ({ product }: ProductCard) => {
+  function handleDownloadProductPhoto(
+    event: React.MouseEvent<HTMLButtonElement>
+  ) {
+    event.preventDefault();
+    console.log(`Downloading photo for product: ${product.productCode}`);
+
+    window.open(`/data/${product.productCode}?product=true`, "_blank");
+  }
+
   return (
     <>
       <div className="flex flex-col rounded-md border-1 border-gray-400/40 bg-white dark:bg-neutral-700 p-6 shadow-md dark:shadow dark:shadow-purple-600 md:flex-row ">
-        <div className="flex size-60 justify-center md:m-5">
+        <div className="relative flex size-60 justify-center md:m-5">
           <img className="rounded-md" src={product.imageUrl} />
+          <button
+            onClick={handleDownloadProductPhoto}
+            className="absolute bottom-0 right-0 cursor-pointer rounded-md bg-[#007bffbd] hover:bg-[#0066ff] p-1 text-white shadow-md transition-colors duration-200 dark:bg-purple-700 dark:hover:bg-purple-600 dark:focus:outline-none dark:focus:ring-0 dark:focus:bg-purple-600 dark:text-neutral-300"
+          >
+            <DownloadIcon className="size-5" />
+          </button>
         </div>
 
         <div className="m-5 hidden h-auto w-px bg-gray-400/40 md:block"></div>
